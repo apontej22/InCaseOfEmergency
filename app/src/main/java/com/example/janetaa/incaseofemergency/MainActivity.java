77b18697ef5d18212fef750424d61e1cb068d4a2
Janet Aponte <japonte@students.mchenry.edu>
2019-01-27 18:14:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/janetaa/incaseofemergency/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.janetaa.incaseofemergency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
